5cd8a4b095ee4a3837d3b4235d73f97e90b490e7
Marcos Rodrigues <75866363+mrStudDev@users.noreply.github.com>
2020-12-18 19:15:36 -0300

    Update index.py

--- index.py
+++ index.py
@@ -4,9 +4,7 @@ from cowpy import cow
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
+        self.Posts
         message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
         self.wfile.write(message.encode())
 
